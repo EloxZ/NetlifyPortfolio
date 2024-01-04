e55346ee77132f197bd12dab8f2eb9f5d19fe709
EloxZ <45355879+EloxZ@users.noreply.github.com>
2024-01-04 16:04:45 +0100

    Frame & Background

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrameComponent } from './components/frame/frame.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FloatingButtonComponent } from './components/floating-button/floating-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrameComponent,
+    NavbarComponent,
+    FloatingButtonComponent
   ],
   imports: [
     BrowserModule,
